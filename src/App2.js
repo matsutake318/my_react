54ff41936a4d37d0400e8b30bb06c009b5d6591c
松井陽志 <TakashiMatsui@takashi.lan>
2018-04-07 10:48:08 +0700

    react

---
+++ src/App2.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+class App2 extends Component {
+  render() {
+    return (
+      <h1>createReact</h1>
+    );
+  }
+}
+
+export default App2;
